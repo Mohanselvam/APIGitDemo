9bd400b50e6410f08a8dbfdffc6b6ec8d6984503
Mohan <mohanselvam2013@gmail.com>
2024-12-08 15:46:05 +0530

    Added Re-Usable Methods

--- src/Files/ReUsableMethods.java
+++ src/Files/ReUsableMethods.java
@@ -4,6 +4,8 @@ import io.restassured.path.json.JsonPath;
 
 public class ReUsableMethods {
 
+        //Adding methods in Reusable of API
+
 	public static JsonPath rawToJson(String response) {
 		JsonPath js1 = new JsonPath(response);
 		return js1;
